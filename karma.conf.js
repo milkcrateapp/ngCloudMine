4272b74a4ca7a3b64842e767dedab2ac88250167
Jason Cox <JasonWilburCox@gmail.com>
2015-09-19 10:11:15 -0400

    Should be all setup now

--- karma.conf.js
+++ karma.conf.js
@@ -10,11 +10,13 @@ module.exports = function(config) {
 
     // frameworks to use
     // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
-    frameworks: ['mocha'],
+    frameworks: ['mocha', 'sinon-chai'],
 
 
     // list of files / patterns to load in the browser
     files: [
+      'lib/angular.js',
+      'lib/*.js',
       'src/*.js',
       'test/*Spec.js'
     ],

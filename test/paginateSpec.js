8079adedadd5f108f1da91e0c42efe9155795ad7
Jason Cox <JasonWilburCox@gmail.com>
2015-12-11 22:54:23 -0500

    More tests

--- test/paginateSpec.js
+++ test/paginateSpec.js
@@ -110,15 +110,18 @@ describe('paginate', function() {
       expect(pager.totalPages).to.equal(3);
       expect(pager.total).to.equal(7);
 
+      wsStub.search.restore();
+      sinon.stub(wsStub, 'search', setCloudmineSuccessResponse([{}, {count: 4}]));
       pager.setQuery('new_query');
       $rootScope.$apply();
       expect(pager.query).to.equal('new_query');
+      expect(pager.total).to.equal(4);
 
       pager.getPage(0);
       $rootScope.$apply();
 
-      expect(wsStub.search.getCall(2).args[0]).to.equal('new_query');
-      expect(wsStub.search.getCall(2).args[1]).to.deep.equal({
+      expect(wsStub.search.getCall(1).args[0]).to.equal('new_query');
+      expect(wsStub.search.getCall(1).args[1]).to.deep.equal({
         limit: 3,
         skip: 0,
         applevel: true
